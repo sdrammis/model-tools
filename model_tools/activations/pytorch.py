e9ad56295b75b033eca2ed60bf1784b58f638da3
Sabrina M Drammis <sdrammis@openmind7.ib.cluster>
2023-02-01 09:57:55 -0500

    Fix bug on as_rbg

--- model_tools/activations/pytorch.py
+++ model_tools/activations/pytorch.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 
 import logging
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 
 from model_tools.activations.core import ActivationsExtractorHelper
 from model_tools.utils import fullname
@@ -105,48 +105,64 @@ class PytorchWrapper:
         return g
 
 
-def load_preprocess_images(image_filepaths, image_size, **kwargs):
-    images = load_images(image_filepaths)
-    images = preprocess_images(images, image_size=image_size, **kwargs)
+def load_preprocess_images(image_filepaths, image_size, as_rgb=True, **kwargs):
+    images = load_images(image_filepaths, as_rgb)
+    images = preprocess_images(images, image_size=image_size, as_rgb=as_rgb, **kwargs)
     return images
 
 
-def load_images(image_filepaths):
-    return [load_image(image_filepath) for image_filepath in image_filepaths]
+def load_images(image_filepaths, as_rgb):
+    return [load_image(image_filepath, as_rgb) for image_filepath in image_filepaths]
 
 
-def load_image(image_filepath):
+def load_image(image_filepath, as_rgb):
     with Image.open(image_filepath) as pil_image:
         if 'L' not in pil_image.mode.upper() and 'A' not in pil_image.mode.upper() \
                 and 'P' not in pil_image.mode.upper():  # not binary and not alpha and not palletized
             # work around to https://github.com/python-pillow/Pillow/issues/1144,
             # see https://stackoverflow.com/a/30376272/2225200
-            return pil_image.copy()
+            if as_rgb:
+                return pil_image.copy()
+            else:
+                return ImageOps.grayscale(pil_image.copy())
         else:  # make sure potential binary images are in RGB
-            rgb_image = Image.new("RGB", pil_image.size)
-            rgb_image.paste(pil_image)
-            return rgb_image
+            if as_rgb:
+                rgb_image = Image.new("RGB", pil_image.size)
+                rgb_image.paste(pil_image)
+                return rgb_image
+            else:
+                return ImageOps.grayscale(pil_image)
 
 
-def preprocess_images(images, image_size, **kwargs):
-    preprocess = torchvision_preprocess_input(image_size, **kwargs)
+def preprocess_images(images, image_size, as_rgb, **kwargs):
+    preprocess = torchvision_preprocess_input(image_size, as_rgb, **kwargs)
     images = [preprocess(image) for image in images]
     images = np.concatenate(images)
     return images
 
 
-def torchvision_preprocess_input(image_size, **kwargs):
+def torchvision_preprocess_input(image_size, as_rgb, **kwargs):
     from torchvision import transforms
+    if type(image_size) is tuple:
+        imsize = image_size
+    else:
+        imsize = (image_size, image_size)
     return transforms.Compose([
-        transforms.Resize((image_size, image_size)),
-        torchvision_preprocess(**kwargs),
+        transforms.Resize(imsize),
+        torchvision_preprocess(as_rgb, **kwargs),
     ])
 
 
-def torchvision_preprocess(normalize_mean=(0.485, 0.456, 0.406), normalize_std=(0.229, 0.224, 0.225)):
+def torchvision_preprocess(as_rgb, normalize_mean=(0.485, 0.456, 0.406), normalize_std=(0.229, 0.224, 0.225)):
     from torchvision import transforms
-    return transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=normalize_mean, std=normalize_std),
-        lambda img: img.unsqueeze(0)
-    ])
+    if as_rgb:
+        return transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=normalize_mean, std=normalize_std),
+            lambda img: img.unsqueeze(0)
+        ])
+    else:
+        return transforms.Compose([
+            transforms.ToTensor(),
+            lambda img: img.unsqueeze(0)
+        ])
